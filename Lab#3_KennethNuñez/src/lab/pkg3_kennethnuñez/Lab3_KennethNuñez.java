398a1afe08b24a8ef9790ffd6be0c3a487e8a207
Kenneth Nuñez <Kenneth11741149>
2018-05-11 16:40:24 -0600

    El programa hasta ahora tiene funcional la creacion de personajes, la eleccion de la dificultad, la funcion de salir

--- Lab#3_KennethNuñez/src/lab/pkg3_kennethnuñez/Lab3_KennethNuñez.java
+++ Lab#3_KennethNuñez/src/lab/pkg3_kennethnuñez/Lab3_KennethNuñez.java
@@ -20,7 +20,66 @@ static Random Random = new Random();
         switch(option){
             case 1:
                 if(dificulty == true){
-                    System.out.println("Ingrese el HP de su Personaje: ");
+                    System.out.println("Ingrese el nombre de su personaje. ");
+                    read.nextLine();
+                    String Nombre = read.nextLine();
+                    System.out.println("Ingrese la edad de su personaje. ");
+                    String Edad = read.nextLine();
+                    System.out.println("Ingrese la estatura del personaje. ");
+                    String Estatura = read.nextLine();
+                    System.out.println("Ingrese el peso de su personaje. ");
+                    String Peso = read.nextLine();
+                    System.out.println("1. Negro. ");
+                    System.out.println("2. Cafe. ");
+                    System.out.println("3. Rubio.");
+                    System.out.println("Escoja su opcion: ");
+                    int pelo = read.nextInt();
+                    String hair = "Negro";
+                    if(pelo == 1){
+                        hair = "Negro";
+                    } else if (pelo == 2){
+                        hair = "Cafe";
+                    } else {
+                        hair = "Rubio";
+                    }
+                    System.out.println("1. Hobbit. ");
+                    System.out.println("2. Elfo. ");
+                    System.out.println("3. Humanos. ");
+                    System.out.println("4. Enanos . ");
+                    int razation = read.nextInt();
+                    String razat = "Humanos";
+                    if(razation == 1){
+                        razat = "Hobbit";
+                    } else if (razation == 2){
+                        razat = "Elfo";
+                    } else if (razation == 3){
+                        razat = "Humanos";
+                    } else{
+                        razat = "Enanos";
+                    }
+                    System.out.println("Ingrese el color de piel de su jugador. ");
+                    String ColorDePiel = read.nextLine();
+                    String ColorDePelo = hair;
+                    String Raza = razat;
+                    System.out.println("Ingrese que personaje desea: ");
+                    System.out.println("1. Arquero. ");
+                    System.out.println("2. Berzerk. ");
+                    System.out.println("3. Picaro. ");
+                    System.out.println("4. Mago. ");
+                    int option3 = read.nextInt();
+                    switch(option3){
+                        case 1:
+                            
+                            break;
+                        case 2:
+                            break;
+                        case 3:
+                            break;
+                        case 4:
+                            break;
+                        default:
+                            System.out.println("Opcion mas que invalida. ");
+                    }
                 } else {
                     System.out.println("No ha elejido la dificultad. ");
                 }
