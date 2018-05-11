4b4dd8bc2f6d63f36f8e93aa2f50a394541cb206
Kenneth Nuñez <Kenneth11741149>
2018-05-11 15:23:56 -0600

    Ahora empieza la creacion de la clase de los enemigos. Ahora que se termino de crear la de los aliados

---
+++ Lab#3_KennethNuñez/src/lab/pkg3_kennethnuñez/Mago.java
@@ -0,0 +1,93 @@
+package lab.pkg3_kennethnuñez;
+import java.util.Random;
+
+public class Mago extends PersonajesAliados {
+   static Random Random = new Random();
+   private int HP = 120;
+   private int AP = 50;
+   private int AD = 20;
+   private int eficacia = 1+Random.nextInt();
+   private int velocidad = 10;
+   private int suerte = 20;
+   private int ProbabilidadDeCritico = 1+Random.nextInt();
+
+    public Mago(String Nombre, String Edad, String Estatura, String Peso, String ColorDePiel, String ColorDePelo, String Raza) {
+        super(Nombre, Edad, Estatura, Peso, ColorDePiel, ColorDePelo, Raza);
+    }
+
+    public static Random getRandom() {
+        return Random;
+    }
+
+    public static void setRandom(Random Random) {
+        Mago.Random = Random;
+    }
+
+    public int getHP() {
+        return HP;
+    }
+
+    public void setHP(int HP) {
+        this.HP = HP;
+    }
+    
+    public int getAP() {
+        return AP;
+    }
+
+    public void setAP(int AP) {
+        this.AP = AP;
+    }
+
+    public int getAD() {
+        return AD;
+    }
+
+    public void setAD(int AD) {
+        this.AD = AD;
+    }
+
+    public int getEficacia() {
+        return eficacia;
+    }
+
+    public void setEficacia(int eficacia) {
+        this.eficacia = eficacia;
+    }
+
+    public int getVelocidad() {
+        return velocidad;
+    }
+
+    public void setVelocidad(int velocidad) {
+        this.velocidad = velocidad;
+    }
+
+    public int getSuerte() {
+        return suerte;
+    }
+
+    public void setSuerte(int suerte) {
+        this.suerte = suerte;
+    }
+
+    public int getProbabilidadDeCritico() {
+        return ProbabilidadDeCritico;
+    }
+
+    public void setProbabilidadDeCritico(int ProbabilidadDeCritico) {
+        this.ProbabilidadDeCritico = ProbabilidadDeCritico;
+    }
+
+    
+    
+    @Override
+    public String toString() {
+        return "Mago{" + "HP=" + HP + ", AP=" + AP + ", AD=" + AD + ", eficacia=" + eficacia + ", velocidad=" + velocidad + ", suerte=" + suerte + ", ProbabilidadDeCritico=" + ProbabilidadDeCritico + '}';
+    }
+    
+    
+
+   
+   
+}
---
+++ Lab#3_KennethNuñez/src/lab/pkg3_kennethnuñez/PersonajesEnemigos.java
@@ -0,0 +1,6 @@
+package lab.pkg3_kennethnuñez;
+
+public class PersonajesEnemigos {
+    String Nombre;
+    String Edad;
+}
